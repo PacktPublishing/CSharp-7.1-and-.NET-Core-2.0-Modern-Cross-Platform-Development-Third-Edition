3c538a8e658dabf34a4c6d24df128348368bdba3
packtBhagyashree <bhagyashreer@packtpub.com>
2017-12-01 16:17:42 +0530

    Code files added

---
+++ VSCode/Part3/NorthwindService/Startup.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.EntityFrameworkCore;
+using NorthwindService.Repositories;
+using Swashbuckle.AspNetCore.Swagger;
+using Packt.CS7;
+
+namespace NorthwindService
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors();
+
+            services.AddDbContext<Northwind>(options =>
+                options.UseSqlite("Data Source=../Northwind.db"));
+
+            services.AddScoped<ICustomerRepository, CustomerRepository>();
+         
+            services.AddMvc();
+
+            // Register the Swagger generator, and define a Swagger document for Northwind service
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "Northwind Service API", Version = "v1" });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseCors(c => c.WithOrigins("http://localhost:5002"));
+
+            app.UseMvc();
+
+            app.UseSwagger();
+            
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Northwind Service API V1");
+            });
+        }
+    }
+}
